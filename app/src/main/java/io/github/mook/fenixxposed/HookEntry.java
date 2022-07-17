acfdab25f7d9b1796267fa674ab1825184f1957d
Mook <mook@users.noreply.github.com>
2022-07-17 09:02:07 -0700

    Disable adding sites to home screen.

--- app/src/main/java/io/github/mook/fenixxposed/HookEntry.java
+++ app/src/main/java/io/github/mook/fenixxposed/HookEntry.java
@@ -1,11 +1,18 @@
 package io.github.mook.fenixxposed;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookConstructor;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClassIfExists;
+import static de.robv.android.xposed.XposedHelpers.findFieldIfExists;
+import static de.robv.android.xposed.XposedHelpers.setBooleanField;
+
+import android.content.Context;
 
 import java.util.Objects;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class HookEntry implements IXposedHookLoadPackage {
@@ -15,6 +22,7 @@ public class HookEntry implements IXposedHookLoadPackage {
             return;
         }
 
+        // Disable highlighting for What's New
         findAndHookMethod(
                 "org.mozilla.fenix.whatsnew.SharedPreferenceWhatsNewStorage",
                 lpparam.classLoader,
@@ -25,5 +33,35 @@ public class HookEntry implements IXposedHookLoadPackage {
                         param.setResult(365);
                     }
                 });
+
+        // Disable adding sites to home screen / install webapp
+        Class<?> defaultToolbarMenu = findClassIfExists(
+                "org.mozilla.fenix.components.toolbar.DefaultToolbarMenu",
+                lpparam.classLoader);
+        if (defaultToolbarMenu != null) {
+            if (findFieldIfExists(defaultToolbarMenu, "isPinningSupported") != null) {
+                // Disable highlighting for Install Web App
+                findAndHookConstructor(
+                        defaultToolbarMenu,
+                        Context.class,
+                        "mozilla.components.browser.state.store.BrowserStore",
+                        boolean.class,
+                        "kotlin.jvm.functions.Function1",
+                        "androidx.lifecycle.LifecycleOwner",
+                        "mozilla.components.concept.storage.BookmarksStorage",
+                        "mozilla.components.feature.top.sites.PinnedSiteStorage",
+                        boolean.class,
+                        new XC_MethodHook() {
+                            @Override
+                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                setBooleanField(param.thisObject, "isPinningSupported", false);
+                            }
+                        });
+            } else {
+                XposedBridge.log("FenixXposed: Could not find DefaultToolbarMenu::isPinningSupported");
+            }
+        } else {
+            XposedBridge.log("FenixXposed: Could not find DefaultToolbarMenu");
+        }
     }
 }
